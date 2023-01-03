81b46c91e78aa4c3178756cfe5432c3bf5620a58
PKS0210CODERED <saanusansaanu@gmail.com>
2023-01-03 14:12:22 -0800

    java file inserted 2.0

--- sum.java
+++ sum.java
@@ -3,5 +3,7 @@ class sum
     public static void main(String args[]){
         System.out.println("hi hi");
         System.out.println("ok then");
+        System.out.println("ok got it");
+
     }
 }
