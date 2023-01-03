fdf78ae5c26995f56847cfe3d63b4732d186aa05
PKS0210CODERED <120698644+PKS0210CODERED@users.noreply.github.com>
2023-01-03 01:07:08 -0800

    latest

--- sum.java
+++ sum.java
@@ -6,7 +6,7 @@ class sum
         System.out.println("ok got it");
         System.out.println("ok got it 2.0");
 
-        System.out.println("naai");
+        System.out.println("last change");
 
     }
 }
